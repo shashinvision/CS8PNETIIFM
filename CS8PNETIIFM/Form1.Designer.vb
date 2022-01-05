57bcf79e2e6a6a260b45055d8f6f6f0de5451cd6
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-01-04 22:14:07 -0300

    FULL

--- CS8PNETIIFM/Form1.Designer.vb
+++ CS8PNETIIFM/Form1.Designer.vb
@@ -25,6 +25,8 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.cargaArchivoDialog = New System.Windows.Forms.OpenFileDialog()
         Me.actividadesGD = New System.Windows.Forms.DataGridView()
+        Me.actividadesTXT = New System.Windows.Forms.Button()
+        Me.actividadesXML = New System.Windows.Forms.Button()
         CType(Me.actividadesGD, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -33,7 +35,7 @@ Partial Class Form1
         Me.Button1.Location = New System.Drawing.Point(58, 42)
         Me.Button1.Margin = New System.Windows.Forms.Padding(2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(90, 27)
+        Me.Button1.Size = New System.Drawing.Size(147, 27)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Cargar XML"
         Me.Button1.UseVisualStyleBackColor = True
@@ -48,11 +50,35 @@ Partial Class Form1
         Me.actividadesGD.Size = New System.Drawing.Size(829, 247)
         Me.actividadesGD.TabIndex = 1
         '
+        'actividadesTXT
+        '
+        Me.actividadesTXT.Enabled = False
+        Me.actividadesTXT.Location = New System.Drawing.Point(247, 42)
+        Me.actividadesTXT.Margin = New System.Windows.Forms.Padding(2)
+        Me.actividadesTXT.Name = "actividadesTXT"
+        Me.actividadesTXT.Size = New System.Drawing.Size(230, 27)
+        Me.actividadesTXT.TabIndex = 2
+        Me.actividadesTXT.Text = "Crear TXT Actividades"
+        Me.actividadesTXT.UseVisualStyleBackColor = True
+        '
+        'actividadesXML
+        '
+        Me.actividadesXML.Enabled = False
+        Me.actividadesXML.Location = New System.Drawing.Point(503, 42)
+        Me.actividadesXML.Margin = New System.Windows.Forms.Padding(2)
+        Me.actividadesXML.Name = "actividadesXML"
+        Me.actividadesXML.Size = New System.Drawing.Size(230, 27)
+        Me.actividadesXML.TabIndex = 3
+        Me.actividadesXML.Text = "Crear XML Actividades"
+        Me.actividadesXML.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(922, 502)
+        Me.ClientSize = New System.Drawing.Size(922, 428)
+        Me.Controls.Add(Me.actividadesXML)
+        Me.Controls.Add(Me.actividadesTXT)
         Me.Controls.Add(Me.actividadesGD)
         Me.Controls.Add(Me.Button1)
         Me.Margin = New System.Windows.Forms.Padding(2)
@@ -66,4 +92,6 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents cargaArchivoDialog As OpenFileDialog
     Friend WithEvents actividadesGD As DataGridView
+    Friend WithEvents actividadesTXT As Button
+    Friend WithEvents actividadesXML As Button
 End Class
--- CS8PNETIIFM/Form1.vb
+++ CS8PNETIIFM/Form1.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Xml
+﻿Imports System.IO
+Imports System.Xml
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         cargaArchivoDialog.ShowDialog()
@@ -11,8 +12,67 @@ Public Class Form1
 
             dataSet.ReadXml(actividadXML)
             actividadesGD.DataSource = dataSet.Tables(0)
+            MessageBox.Show("Carga de XML: " + cargaArchivoDialog.FileName + " correcta")
+            actividadesTXT.Enabled = True
+            actividadesXML.Enabled = True
 
         End If
 
     End Sub
+
+    Private Sub actividadesTXT_Click(sender As Object, e As EventArgs) Handles actividadesTXT.Click
+        Dim escritorTxt As TextWriter = New StreamWriter("C:\Control8\actividades_felipe_mancilla.txt", True, System.Text.Encoding.UTF8)
+
+        For Each row As DataGridViewRow In actividadesGD.Rows
+            escritorTxt.WriteLine("id_actividad: " + row.Cells("id_actividad").Value)
+            escritorTxt.WriteLine("codsala: " + row.Cells("codsala").Value)
+            escritorTxt.WriteLine("nomactividad: " + row.Cells("nomactividad").Value)
+            ' escritorTxt.WriteLine("url: " + row.Cells("url").Value)
+            escritorTxt.WriteLine("descripcion: " + row.Cells("descripcion").Value)
+
+
+            escritorTxt.WriteLine("------")
+        Next
+
+        escritorTxt.Close()
+        MessageBox.Show("Archivo TXT: C:\Control8\actividades_felipe_mancilla.txt creado de forma correcta")
+    End Sub
+
+
+    Private Sub actividadesXML_Click(sender As Object, e As EventArgs) Handles actividadesXML.Click
+
+        Dim escritorXml As XmlWriter = XmlWriter.Create("C:\Control8\actividades_felipe_mancilla.xml")
+        escritorXml.WriteStartDocument()
+        escritorXml.WriteStartElement("Actividades")
+
+        Dim valorEntrada As Integer
+
+        For row As Integer = 0 To actividadesGD.Rows.Count - 1
+
+            valorEntrada = numAleatorioEntre(1000, 5000)
+
+            If actividadesGD.Rows.Item(row).Cells(0).Value <> "" Then
+                escritorXml.WriteStartElement("Actividad")
+                escritorXml.WriteElementString("id_actividad", actividadesGD.Rows.Item(row).Cells(0).Value)
+                escritorXml.WriteElementString("url", actividadesGD.Rows.Item(row).Cells(2).Value)
+                escritorXml.WriteElementString("nomactividad", actividadesGD.Rows.Item(row).Cells(1).Value)
+                escritorXml.WriteElementString("descripcion", actividadesGD.Rows.Item(row).Cells(3).Value)
+                escritorXml.WriteElementString("valor_entrada", valorEntrada)
+
+                escritorXml.WriteEndElement()
+            End If
+        Next
+
+        escritorXml.WriteEndElement()
+        escritorXml.WriteEndDocument()
+        escritorXml.Close()
+        MessageBox.Show("Archivo XML: C:\Control8\actividades_felipe_mancilla.xml creado de forma correcta")
+
+
+    End Sub
+
+    Function numAleatorioEntre(ByVal minimo As Integer, ByVal maximo As Integer) As Integer
+        Randomize()
+        Return CLng((minimo - maximo) * Rnd() + maximo)
+    End Function
 End Class
